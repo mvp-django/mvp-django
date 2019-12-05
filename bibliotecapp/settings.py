5d4454362242cbbad466ba9733939d146b72c6bb
Marcos Paulo Diniz <marcos.diniz@4linux.com.br>
2019-12-05 11:51:28 -0300

    corrigindo o erro da estagiaria

--- bibliotecapp/settings.py
+++ bibliotecapp/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '2&_qrw)6v7loa*w6z3gxeb8*f7e0y(0@pxz^rlk5_0xh)_4k%e'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -121,7 +121,7 @@ USE_TZ = True
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static-files')
 #DataFlair #User_Uploaded_Files
-MEDIA_URL = 'media/'
+MEDIA_URL = '/media/'
 MEDIA_ROOT=os.path.join(BASE_DIR, 'media')
 LOGIN_REDIRECT_URL = '/'
 LOGOUT_REDIRECT_URL = 'login'

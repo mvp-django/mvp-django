aa2cea9751884b265e70e2984a7b6d19b06af7b5
Marcos Paulo Diniz <marcos.diniz@4linux.com.br>
2019-12-05 17:05:09 -0300

    corrigindo a funçao de upload e adicionando a funçao de download

--- livro/views.py
+++ livro/views.py
@@ -1,7 +1,10 @@
+import os
 from django.shortcuts import render, redirect
 from livro.models import Livro
 from livro.forms import LivroCreate
-from django.http import HttpResponse
+from django.http import HttpResponse, Http404
+from bibliotecapp.settings import STATIC_URL, STATIC_ROOT, MEDIA_URL, MEDIA_ROOT
+
 
 # Create your views here.
 def index(request):
@@ -26,9 +29,12 @@ def update_livro(request, livro_id):
     except Livro.DoesNotExist:
         return redirect('index')
     livro_form = LivroCreate(request.POST or None, instance = livro_sel)
-    if livro_form.is_valid():
-       livro_form.save()
-       return redirect('index')
+
+    if request.method == 'POST':
+        livro_form = LivroCreate(request.POST, request.FILES, instance = livro_sel)
+        if livro_form.is_valid():
+            livro_form.save()
+        return redirect('index')
     return render(request, 'livro/upload_form.html', {'upload_form':livro_form})
 
 def delete_livro(request, livro_id):
@@ -40,6 +46,21 @@ def delete_livro(request, livro_id):
     livro_sel.delete()
     return redirect('index')
 
+def download(request, livro_id):
+    livro_id = int(livro_id)
+    try:
+        livro_sel = Livro.objects.get(id = livro_id)
+    except Livro.DoesNotExist:
+        return redirect('index')
+
+    file_path = os.path.join(MEDIA_ROOT, livro_sel.document.url)
+    file_path = os.getcwd() + file_path
+    if os.path.exists(file_path):
+        with open(file_path, 'rb') as fh:
+            response = HttpResponse(fh.read(), content_type="application/vnd.ms-excel")
+            response['Content-Disposition'] = 'inline; filename=' + os.path.basename(file_path)
+            return response
+    raise Http404
 
 def error_404(request, exception):
     data = {}

4f4b740ef4bb9a7f52a6799f2a4d55dd7b5f3d8e
Marcos Paulo Diniz <marcos.diniz@4linux.com.br>
2019-12-05 11:58:01 -0300

    adicionando documento no models

--- livro/models.py
+++ livro/models.py
@@ -3,7 +3,7 @@ class Livro(models.Model):
     nome = models.CharField(max_length = 50)
     figura = models.ImageField()
     autor = models.CharField(max_length = 30, default='anonymous')
-    # document = models.FileField(upload_to='documents/')
+    document = models.FileField(upload_to='documents/')
     email = models.EmailField(blank = True)
     descricao = models.TextField(default = '4Linux Django')
     def __str__(self):
